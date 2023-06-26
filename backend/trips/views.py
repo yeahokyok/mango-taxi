fb7c49f50927a2b473206b043fd6349e8c0d5904
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-06-26 12:58:18 +0700

    retrieve trip api

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -8,3 +8,5 @@ class TripView(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "trip_id"
