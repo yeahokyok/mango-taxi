8d314751945a32310aae5784098176f5cb30da2f
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-06-26 14:55:31 +0700

    TestWebSocket  test_can_connect_to_server

--- backend/taxi/asgi.py
+++ backend/taxi/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
+from trips.consumers import TaxiConsumer
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi.settings")
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": URLRouter(
+            [
+                path("taxi/", TaxiConsumer.as_asgi()),
+            ]
+        ),
+    }
+)
