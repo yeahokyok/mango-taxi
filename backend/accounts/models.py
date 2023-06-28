05a2ffa0f78db0c2ce456e5181122d2f86edb31a
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-06-28 14:37:19 +0700

    add group to user serializer

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,4 +2,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    @property
+    def group(self):
+        groups = self.groups.all()
+        return groups[0].name if groups else None
