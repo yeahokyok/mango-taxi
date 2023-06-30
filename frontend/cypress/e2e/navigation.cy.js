567c801cb68cf0ebfd21615048bbf91f12a31c5e
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-06-30 12:41:04 +0700

    test routes

--- frontend/cypress/e2e/navigation.cy.js
+++ frontend/cypress/e2e/navigation.cy.js
@@ -4,4 +4,33 @@ describe("Navigation", function () {
         cy.get("a").contains("Sign up").click()
         cy.hash().should("eq", "#/signup")
     })
+    it("Can navigate to log in from home", function () {
+        cy.visit("/#/")
+        cy.get("a").contains("Log in").click()
+        cy.hash().should("eq", "#/login")
+    })
+
+    it("Can navigate to home from sign up", function () {
+        cy.visit("/#/signup")
+        cy.get("a").contains("Home").click()
+        cy.hash().should("eq", "#/")
+    })
+
+    it("Can navigate to log in from sign up", function () {
+        cy.visit("/#/signup")
+        cy.get("a").contains("Log in").click()
+        cy.hash().should("eq", "#/login")
+    })
+
+    it("Can navigate to home from log in", function () {
+        cy.visit("/#/login")
+        cy.get("a").contains("Home").click()
+        cy.hash().should("eq", "#/")
+    })
+
+    it("Can navigate to sign up from log in", function () {
+        cy.visit("/#/login")
+        cy.get("a").contains("Sign up").click()
+        cy.hash().should("eq", "#/signup")
+    })
 })
